46e878b910c76fa55518e1dea2cb02f0385edfcf
IainDelaney <iain.delaney@gmail.com>
2022-11-02 11:25:17 -0400

    Initial commit, refactored out of Content View.

---
+++ Cloudy2/DayView.swift
@@ -0,0 +1,31 @@
+//
+//  DayView.swift
+//  Cloudy2
+//
+//  Created by Iain Delaney on 2022-11-02.
+//  Copyright © 2022 IainDelaney. All rights reserved.
+//
+
+import SwiftUI
+
+struct DayView: View {
+    var weather: DailyWeather
+    var image: UIImage
+    var body: some View {
+        GeometryReader { geometry in
+            VStack(alignment: .center) {
+                CloudView(weather: self.weather, image: self.image)
+                Text(self.weather.dateString)
+                    .font(.headline)
+            }
+            .frame(width: geometry.size.width/2, height: 185.0)
+        }
+        .background(Color.blue)
+    }
+}
+
+struct DayView_Previews: PreviewProvider {
+    static var previews: some View {
+        DayView(weather:DailyWeather() , image: UIImage(named:"preview_icon")!)
+    }
+}
