96f37f23e9afe9e372fb2807d4f880bd9b647f31
IainDelaney <iain.delaney@gmail.com>
2022-11-05 12:32:25 -0400

    remove geometry reader.

--- Cloudy2/DayView.swift
+++ Cloudy2/DayView.swift
@@ -12,13 +12,10 @@ struct DayView: View {
     var weather: DailyWeather
     var image: UIImage
     var body: some View {
-        GeometryReader { geometry in
-            VStack(alignment: .center) {
+            VStack(spacing: 0) {
                 CloudView(weather: self.weather, image: self.image)
                 Text(self.weather.dateString)
                     .font(.headline)
-            }
-            .frame(width: geometry.size.width/2, height: 185.0)
         }
     }
 }
